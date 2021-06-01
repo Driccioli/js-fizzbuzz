4097ebb1c88b2f4c6580acbd6389e78f7a101c9b
Driccioli <driccioli53@gmail.com>
2021-06-01 15:50:20 +0200

    Primo commit

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+var output = document.getElementById("output");
+
+for(var i=1; i<100; i++);{
+    // if((i % 3)==0){
+    //     output.innerHTML +="Fizz";
+    // } else if((i%5)==0){
+    //     output.innerHTML +="Buzz";
+    // }  else(((i%3) !=0)&&((i%5) !=0)){
+    //     output.innerHTML +=i;
+    // }
+    output.innerHTML += i;
+    // output.innerHTML += "<br>"
+}
